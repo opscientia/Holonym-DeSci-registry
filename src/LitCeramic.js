6c472c21b82299263b252fe134e9438e7bf1f606
nanaknihal <nanaknihal@gmail.com>
2022-03-20 22:52:29 -0400

    lit/ceramic integration with evm control working

--- src/LitCeramic.js
+++ src/LitCeramic.js
@@ -12,58 +12,109 @@ const signer = provider.getSigner()
 let litCeramicIntegration = new Integration('https://ceramic-clay.3boxlabs.com', 'mumbai')
 litCeramicIntegration.startLitClient(window)
 
-// const accessControlConditions = [
+// const accessControlConditions = [{
+//     contractAddress: '',
+//     standardContractType: '',
+//     chain: 'mumbai',
+//     method: 'eth_getBalance',
+//     parameters: [
+//       ':userAddress',
+//       'latest'
+//     ],
+//     returnValueTest: {
+//       comparator: '>=',
+//       value: '1'
+//     }
+//   }]
+
+
+
+// const evmContractConditions = [
 //     {
 //         contractAddress: contractAddresses['google'],
-//         standardContractType: '',
-//         chain: 'avalanche',
-//         method: 'hasAccess',
-//         parameters: [
-//         ':userAddress',
-//         ],
+//         functionName: 'hasAccess',
+//         functionParams: [
+//             '0xC8834C1FcF0Df6623Fc8C8eD25064A4148D99388',
+//             '0xC8834C1FcF0Df6623Fc8C8eD25064A4148D99388',
+//             ],
+//         functionAbi: {
+//             "inputs": [
+//               {
+//                 "internalType": "address",
+//                 "name": "owner",
+//                 "type": "address"
+//               },
+//               {
+//                 "internalType": "address",
+//                 "name": "viewer",
+//                 "type": "address"
+//               }
+//             ],
+//             "name": "hasAccess",
+//             "outputs": [
+//               {
+//                 "internalType": "bool",
+//                 "name": "result",
+//                 "type": "bool"
+//               }
+//             ],
+//             "stateMutability": "view",
+//             "type": "function"
+//           },
 //         returnValueTest: {
-//         comparator: '=',
-//         value: 'true'
+//             key: "result",
+//             comparator: "=",
+//             value: "true"
 //         }
 //     }
 // ]
 
-// const accessControlConditions = [
-//     {
-//         contractAddress: contractAddresses['google'],
-//         standardContractType: '',
-//         chain: 'mumbai',
-//         method: 'hasAccess',
-//         parameters: [
-//         ':userAddress',
-//         ],
-//         returnValueTest: {
-//         comparator: '=',
-//         value: 'true'
-//         }
-//     }
-// ]
 
-const accessControlConditions = [{
-    contractAddress: '',
-    standardContractType: '',
-    chain: 'mumbai',
-    method: 'eth_getBalance',
-    parameters: [
-      ':userAddress',
-      'latest'
-    ],
-    returnValueTest: {
-      comparator: '>=',
-      value: '1'
-    }
-  }]
+
+const evmContractConditions = [
+    {
+      contractAddress: "0x1362fe03c3c338f2e7dfaA44205f2B047f2C430D",
+      functionName: "hasAccess",
+      functionParams: [":userAddress",":userAddress"],
+      functionAbi: {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "owner",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "viewer",
+            "type": "address"
+          }
+        ],
+        "name": "hasAccess",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "result",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      chain: "mumbai",
+      returnValueTest: {
+        key: "result",
+        comparator: "=",
+        value: "true",
+      },
+    },
+  ];
 
 const stringToEncrypt = 'This is what we want to encrypt on Lit and then store on ceramic'
-
-const grantAccess = async (to) => {
+// Changes to's access to newState (true or false)
+const changeAccess = async (to, newState) => {
+    console.log(contractAddresses['google'])
     let vjwt = new ethers.Contract(contractAddresses['google'], abi, signer)
-    vjwt.setAccess(to, true)
+    vjwt.setAccess(to, newState)
 }
 
 export const LitCeramic = (props)=> {
@@ -71,6 +122,7 @@ export const LitCeramic = (props)=> {
     const [streamContent, setStreamContent] = useState(null)
     const [account, setAccount] = useState(null)
     const [self, setSelf] = useState(null)
+    const [buttonPressed, setButtonPressed] = useState(0) // this should be incremented (or somehow changed) on every button press so it triggers a useEffect
     // get ethereum account
     window.ethereum.request({
     method: 'eth_requestAccounts',
@@ -79,20 +131,25 @@ export const LitCeramic = (props)=> {
 
     // upload content to stream when page loads
     const uploadPrivateCredentials = ()=>{
-        litCeramicIntegration.encryptAndWrite(stringToEncrypt, accessControlConditions).then(streamID => setStreamID(streamID))
+        litCeramicIntegration.encryptAndWrite(stringToEncrypt, evmContractConditions, 'evmContractConditions').then(streamID => setStreamID(streamID))
     }
 
     // load stream from ceramic when streamID changes
     useEffect(()=>{
         if(streamID){
             litCeramicIntegration.readAndDecrypt(streamID).then(c=>setStreamContent(c))
-            console.log('NOODLE')
-            console.log(streamID)
+            let vjwt = new ethers.Contract(contractAddresses['google'], abi, signer)
+            console.log(account)
+            vjwt.hasAccess('0xC8834C1FcF0Df6623Fc8C8eD25064A4148D99388',
+            '0xC8834C1FcF0Df6623Fc8C8eD25064A4148D99388',).then(x=>console.log('HAS AXIS ', x))
         }
-    }, [streamID])
+    }, [streamID, buttonPressed])
+
     return <>
-        <button onClick={async ()=> await grantAccess(account)}>Grant Me Access</button>
+        <button onClick={async ()=> await changeAccess(account, true)}>Grant Me Access</button>
+        <button onClick={async ()=> await changeAccess(account, false)}>Revoke My Access</button>
         <button onClick={uploadPrivateCredentials}>Upload Private Credentials</button>
+        <button onClick={()=>setButtonPressed( buttonPressed +1 ) }>View Credentials</button>
         {streamID ? `Saved to streamID ${streamID} ,` : null} 
         <i>Content :</i>{streamContent}
     </>
