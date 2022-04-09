8f56782ede75a6d491f58e6f91dde09a019d30e9
nanaknihal <nanaknihal@gmail.com>
2022-04-09 11:31:20 -0400

    fixed authentication flow mobile layout

--- src/components/authentication-flow.js
+++ src/components/authentication-flow.js
@@ -75,7 +75,7 @@ const parseJWT = (JWT) => {
 
 const MessageScreen = (props) => {
     return <div className='bg-img x-section wf-section' style={{width:'100vw'}}>
-                <div className="x-container w-container">
+                <div className="x-container w-container" style={{marginTop:'200px'}}>
                     <h3>{props.msg}</h3>
                 </div>
             </div>
