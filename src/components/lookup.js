7fef68eda6416acbd09296278842f6abc312df90
nanaknihal <nanaknihal@gmail.com>
2022-04-09 00:46:26 -0400

    search bar refactoring

--- src/components/lookup.js
+++ src/components/lookup.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Navigate, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 import contractAddresses from '../contractAddresses.json'
 import abi from '../abi/VerifyJWT.json'
 import { InfoButton } from './info-button';
-import { userInfo } from 'os';
+import { SearchBar } from './search-bar';
 import Github from '../img/Github.svg';
 import Google from '../img/Google.svg';
 import CircleWavy from '../img/CircleWavy.svg';
@@ -24,85 +24,6 @@ const icons = {
 }
 const { ethers } = require('ethers');  
   
-const SearchBar = () => {
-    // const searchBarStyle = {
-    //     height : '45px',
-    //     width : '364px',
-    //     color: 'grey',
-    //     fontSize: '19px',
-    //     background : 'transparent',
-    //     border : 'none',
-    // }
-    // const searchButtonStyle = {
-    //     height : '45px',
-    //     width : '45px',
-    //     background : 'yellow',
-    //     color: 'grey',
-    //     fontSize: '19px',
-    //     border : 'none',
-    //     // borderLeft : '5px dotted grey',
-        
-    // }
-    let navigate = useNavigate()
-    let params = useParams()
-    let [credentials, setCredentials] = useState('')
-    // let [web2service, setWeb2Service] = useState(params.web2service)
-    // const toggles = [
-    //     { name: 'Google', value: 'google' },
-    //     { name: 'ORCID', value: 'orcid' },
-    //   ];
-
-    const search = () => {
-        let web2Service = params.web2service;
-
-        if(credentials.startsWith('@')){
-            web2Service = 'Twitter'
-        } else if(credentials.includes('@')){
-            web2Service = 'Google'
-        } else if(credentials.includes('-')){
-            web2Service = 'ORCID'
-        }
-        navigate(`/lookup/${web2Service}/${credentials || 'nobody'}`)
-    }
-    return <>
-            {/* <ButtonGroup>
-                {toggles.map((toggle, idx) => (
-                <ToggleButton
-                    key={idx}
-                    id={`radio-${idx}`}
-                    type="radio"
-                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-                    name='radio'
-                    value={toggle.value}
-                    checked={web2service === toggle.value}
-                    onChange={(e) => setWeb2Service(e.currentTarget.value)}
-                >
-                    {toggle.name}
-                </ToggleButton>
-                ))}
-            </ButtonGroup> */}
-            {/* <span style={{border:'5px dotted grey'}}>
-                <input placeholder='Search for someone' value={credentials} onChange={e=>setCredentials(e.target.value)} style={searchBarStyle} />
-                <button onClick={()=>navigate(`/lookup/${web2service}/${credentials}`)} style={searchButtonStyle}>Go</button>
-            
-            </span> */}
-
-        <div class="optin-form w-form">
-            {/* TODO : this need not be a <form />*/}
-          <form id="email-form" name="email-form" data-name="Email Form" method="get" class="form">
-              <input onChange={e=>setCredentials(e.target.value)} type="email" class="text-field w-input" maxLength="256" name="email-3" data-name="Email 3" placeholder="Discover others by email, Twitter, etc." id="email-3" required="" />
-            </form>
-        </div>
-        <div class="spacer-small"></div>
-        <div class="btn-wrapper">
-          <a onClick={search} class="x-button w-button">search now</a>
-          <div class="v-spacer-small"></div>
-          <div class="spacer-small mobile"></div>
-          <a href="#" class="x-button secondary outline w-button">learn more</a>
-        </div>
-        <div class="spacer-large"></div>
-        </>
-}
 const sendCrypto = (signer, to) => {
     if(!signer || !to) {
         alert('Error! make sure MetaMask is set to Avalanche C testnet and you specify a recipient')
---
+++ src/components/search-bar.js
@@ -0,0 +1,36 @@
+import { useParams, Navigate, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+
+export const SearchBar = () => {
+    let navigate = useNavigate()
+    let params = useParams()
+    let [credentials, setCredentials] = useState('')
+    const search = () => {
+        let web2Service = params.web2service;
+
+        if(credentials.startsWith('@')){
+            web2Service = 'Twitter'
+        } else if(credentials.includes('@')){
+            web2Service = 'Google'
+        } else if(credentials.includes('-')){
+            web2Service = 'ORCID'
+        }
+        navigate(`/lookup/${web2Service}/${credentials || 'nobody'}`)
+    }
+    return <>
+        <div class="optin-form w-form">
+            {/* TODO : this need not be a <form />*/}
+          <form id="email-form" name="email-form" data-name="Email Form" method="get" class="form">
+              <input onChange={e=>setCredentials(e.target.value)} type="email" class="text-field w-input" maxLength="256" name="email-3" data-name="Email 3" placeholder="Discover others by email, Twitter, etc." id="email-3" required="" />
+            </form>
+        </div>
+        <div class="spacer-small"></div>
+        <div class="btn-wrapper">
+          <a onClick={search} class="x-button w-button">search now</a>
+          <div class="v-spacer-small"></div>
+          <div class="spacer-small mobile"></div>
+          <a href="#" class="x-button secondary outline w-button">learn more</a>
+        </div>
+        <div class="spacer-large"></div>
+        </>
+}
