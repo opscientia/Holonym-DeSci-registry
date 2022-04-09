025f4b0e1a0ff9407f02cfea98a4b67fd9037fd4
nanaknihal <nanaknihal@gmail.com>
2022-04-09 14:18:59 -0400

    error component

---
+++ src/components/error.js
@@ -0,0 +1,9 @@
+const Error = (props) => {
+    return <div className='bg-img x-section wf-section' style={{width:'100vw'}}>
+                <div className="x-container w-container" style={{display : "block"}}>
+                    <h3 className="h3">Error</h3>
+                    <p style={{color: "red"}}>{props.msg}</p>
+                </div>
+            </div>
+}
+export default Error;
