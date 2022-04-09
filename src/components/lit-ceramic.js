761f9243dbc3397665b7a3d07f535e876c856907
nanaknihal <nanaknihal@gmail.com>
2022-04-09 15:58:03 -0400

    commented lit ceramic for now for faster loading

--- src/components/lit-ceramic.js
+++ src/components/lit-ceramic.js
@@ -1,104 +1,106 @@
-import React, { useState, useEffect } from 'react'
-import { Integration } from 'lit-ceramic-sdk'
-import contractAddresses from '../contractAddresses.json'
-import abi from '../abi/VerifyJWT.json'
-import { providers } from 'ethers'
-const { ethers } = require('ethers')
+// import React, { useState, useEffect } from 'react'
+// import { Integration } from 'lit-ceramic-sdk'
+// import contractAddresses from '../contractAddresses.json'
+// import abi from '../abi/VerifyJWT.json'
+// import { providers } from 'ethers'
+// const { ethers } = require('ethers')
 
-// const provider = new ethers.providers.Web3Provider(window.ethereum)
-// const signer = provider.getSigner()
+// // const provider = new ethers.providers.Web3Provider(window.ethereum)
+// // const signer = provider.getSigner()
 
-let litCeramicIntegration = new Integration('https://ceramic-clay.3boxlabs.com', 'mumbai')
-litCeramicIntegration.startLitClient(window)
+// let litCeramicIntegration = new Integration('https://ceramic-clay.3boxlabs.com', 'mumbai')
+// litCeramicIntegration.startLitClient(window)
 
-const evmContractConditions = [
-    {
-      contractAddress: "0x1362fe03c3c338f2e7dfaA44205f2B047f2C430D",
-      functionName: "hasAccess",
-      functionParams: [":userAddress",":userAddress"],
-      functionAbi: {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "owner",
-            "type": "address"
-          },
-          {
-            "internalType": "address",
-            "name": "viewer",
-            "type": "address"
-          }
-        ],
-        "name": "hasAccess",
-        "outputs": [
-          {
-            "internalType": "bool",
-            "name": "result",
-            "type": "bool"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      chain: "mumbai",
-      returnValueTest: {
-        key: "result",
-        comparator: "=",
-        value: "true",
-      },
-    },
-  ];
+// const evmContractConditions = [
+//     {
+//       contractAddress: "0x1362fe03c3c338f2e7dfaA44205f2B047f2C430D",
+//       functionName: "hasAccess",
+//       functionParams: [":userAddress",":userAddress"],
+//       functionAbi: {
+//         "inputs": [
+//           {
+//             "internalType": "address",
+//             "name": "owner",
+//             "type": "address"
+//           },
+//           {
+//             "internalType": "address",
+//             "name": "viewer",
+//             "type": "address"
+//           }
+//         ],
+//         "name": "hasAccess",
+//         "outputs": [
+//           {
+//             "internalType": "bool",
+//             "name": "result",
+//             "type": "bool"
+//           }
+//         ],
+//         "stateMutability": "view",
+//         "type": "function"
+//       },
+//       chain: "mumbai",
+//       returnValueTest: {
+//         key: "result",
+//         comparator: "=",
+//         value: "true",
+//       },
+//     },
+//   ];
 
-export const LitCeramic = (props)=> {
-    const [streamID, setStreamID] = useState(null)
-    const [streamContent, setStreamContent] = useState(null)
-    const [account, setAccount] = useState(null)
-    const [self, setSelf] = useState(null)
-    const [buttonPressed, setButtonPressed] = useState(0) // this should be incremented (or somehow changed) on every button press so it triggers a useEffect
-    // get ethereum account
-    props.provider.request({
-    method: 'eth_requestAccounts',
-    })
-    .then(accounts=>setAccount(accounts[0]))
+// export const LitCeramic = (props)=> {
+//     const [streamID, setStreamID] = useState(null)
+//     const [streamContent, setStreamContent] = useState(null)
+//     const [account, setAccount] = useState(null)
+//     const [self, setSelf] = useState(null)
+//     const [buttonPressed, setButtonPressed] = useState(0) // this should be incremented (or somehow changed) on every button press so it triggers a useEffect
+//     // get ethereum account
+//     props.provider.request({
+//     method: 'eth_requestAccounts',
+//     })
+//     .then(accounts=>setAccount(accounts[0]))
 
-    // upload content to stream when page loads
-    const uploadPrivateCredentials = ()=>{
-        litCeramicIntegration.encryptAndWrite(props.stringToEncrypt, evmContractConditions, 'evmContractConditions').then(streamID => setStreamID(streamID))
-    }
+//     // upload content to stream when page loads
+//     const uploadPrivateCredentials = ()=>{
+//         litCeramicIntegration.encryptAndWrite(props.stringToEncrypt, evmContractConditions, 'evmContractConditions').then(streamID => setStreamID(streamID))
+//     }
 
-    // Changes to's access to newState (true or false)
-    const changeAccess = async (to, newState) => {
-      console.log(contractAddresses['google'])
-      let vjwt = new ethers.Contract(contractAddresses['google'], abi, props.provider.getSigner())
-      vjwt.setAccess(to, newState)
-    }
+//     // Changes to's access to newState (true or false)
+//     const changeAccess = async (to, newState) => {
+//       console.log(contractAddresses['google'])
+//       let vjwt = new ethers.Contract(contractAddresses['google'], abi, props.provider.getSigner())
+//       vjwt.setAccess(to, newState)
+//     }
 
-    // Decodes a base64url JWT
-    const decodeJWT = (jwt)=>{
-      if(!jwt){return}
-      console.log('decode', jwt)
-      return jwt.split('.').map(x=>atob(x)).join(',')
-    }
+//     // Decodes a base64url JWT
+//     const decodeJWT = (jwt)=>{
+//       if(!jwt){return}
+//       console.log('decode', jwt)
+//       return jwt.split('.').map(x=>atob(x)).join(',')
+//     }
 
-    // load stream from ceramic when streamID changes or 'View Credentials' is pressed
-    useEffect(()=>{
-        if(streamID){
-            try {
-                litCeramicIntegration.readAndDecrypt(streamID).then(c=>setStreamContent(c))
-            } catch {
-                setStreamContent('Error: You don\'t have access to it') // this never gets called javascript error handling is terrible - hacky workaround is done instead of this
-            }
+//     // load stream from ceramic when streamID changes or 'View Credentials' is pressed
+//     useEffect(()=>{
+//         if(streamID){
+//             try {
+//                 litCeramicIntegration.readAndDecrypt(streamID).then(c=>setStreamContent(c))
+//             } catch {
+//                 setStreamContent('Error: You don\'t have access to it') // this never gets called javascript error handling is terrible - hacky workaround is done instead of this
+//             }
             
-        }
-    }, [streamID, buttonPressed])
+//         }
+//     }, [streamID, buttonPressed])
 
-    return <>
-        <button class='cool-button' onClick={uploadPrivateCredentials}>Finish Uploading Your Private Credentials</button>
-        then
-        <button class='cool-button' onClick={async ()=> await changeAccess(account, true)}>Grant (Me) Access</button>
-        <button class='cool-button' onClick={async ()=> await changeAccess(account, false)}>Revoke (My) Access</button>
-        <button class='cool-button' onClick={()=>setButtonPressed( buttonPressed +1 ) }>View Credentials</button>
-        {streamID ? `Saved to streamID ${streamID} ,` : null} 
-        <i>Content :</i>{streamContent && streamContent.startsWith('something went wrong decrypting:') ? <b>Error: Could not access the content</b> : <p style={{fontSize:'12px'}}>{decodeJWT(streamContent)}</p>}
-    </>
-}
+//     return <>
+//         <button class='cool-button' onClick={uploadPrivateCredentials}>Finish Uploading Your Private Credentials</button>
+//         then
+//         <button class='cool-button' onClick={async ()=> await changeAccess(account, true)}>Grant (Me) Access</button>
+//         <button class='cool-button' onClick={async ()=> await changeAccess(account, false)}>Revoke (My) Access</button>
+//         <button class='cool-button' onClick={()=>setButtonPressed( buttonPressed +1 ) }>View Credentials</button>
+//         {streamID ? `Saved to streamID ${streamID} ,` : null} 
+//         <i>Content :</i>{streamContent && streamContent.startsWith('something went wrong decrypting:') ? <b>Error: Could not access the content</b> : <p style={{fontSize:'12px'}}>{decodeJWT(streamContent)}</p>}
+//     </>
+// }
+
+export const LitCeramic = () => <></>
