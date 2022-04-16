160b4c64f9be29fd66d9ea773c6a4ee060dfc984
nanaknihal <nanaknihal@gmail.com>
2022-04-16 11:01:42 -0400

    clicking address now does different function depending on the page

--- src/components/address.js
+++ src/components/address.js
@@ -1,6 +1,13 @@
 import { truncateAddress } from '../ui-helpers';
 import userIcon from "../img/User.svg";
 
+// profile pic and truncated address
+const InnerContent = (props) =>
+<>
+    <img src={userIcon} loading="lazy" alt="" class="wallet-icon" />
+    <div class="wallet-text">{truncateAddress(props.address)}</div>
+</>
+
 const Address = (props)=>{
     const switchAccounts = () =>{
         if(props.provider && props.provider.provider.request){
@@ -13,14 +20,21 @@ const Address = (props)=>{
         }
     }
 
-
+    const onMyHolo = window.location.href.endsWith('myholo') 
+    console.log('onMyHolo', onMyHolo)
     return <div class="nav-btn">
                 {/* <a href="#" class="x-button secondary outline-menu w-button">connect wallet</a> */}
                 {/* <div onClick={switchAccounts} className="wallet-connected"> */}
-                <a href='/myholo' className="wallet-connected">
-                    <img src={userIcon} loading="lazy" alt="" class="wallet-icon" />
-                    <div class="wallet-text">{truncateAddress(props.address)}</div>
-                </a>
+                {onMyHolo ? 
+                    <div onClick={switchAccounts} className="wallet-connected">
+                        <InnerContent {...props} />
+                    </div>
+                    : 
+                    <a href='/myholo' className='wallet-connected'>
+                        <InnerContent {...props} />
+                    </a> 
+                }
+                
                 {/* <a href='/myholo' class='nav-link w-nav-link'>My Holo</a> */}
 
             </div>
