c57aa5fcf57dfe109f1563cd526149221e9f51b8
nanaknihal <nanaknihal@gmail.com>
2022-05-25 18:31:31 -0400

    better error style on mobile

--- src/components/errors.js
+++ src/components/errors.js
@@ -3,7 +3,7 @@ import React from "react";
 const Error = (props) => {
   return (
     <div className="bg-img x-section wf-section" style={{ width: "100vw" }}>
-      <div className="x-container w-container" style={{ display: "block" }}>
+      <div className="x-container w-container" style={{ display: "block", marginTop: "100px" }}>
         <h3 className="h3">Error</h3>
         <div className="spacer-small"></div>
         <p style={{ color: "red" }}>{props.msg}</p>
