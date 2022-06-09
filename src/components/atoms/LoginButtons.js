397ac43bfb4c98b2b37abf1c95d444b963b51e78
nanaknihal <nanaknihal@gmail.com>
2022-06-09 19:57:26 -0400

    new login buttons prompt for new chain first

--- src/components/atoms/LoginButtons.js
+++ src/components/atoms/LoginButtons.js
@@ -16,71 +16,93 @@ const LoginButton = (props) => {
     </>
     )
 }
+/** ORCID **/
 export const ORCIDLoginButton = (props) => {
-  console.log("lorcidp", props)
   return <LoginButton 
     web2service={"ORCID"} 
     callback={(newChain)=>{window.location.href=`https://orcid.org/signin?response_type=token&redirect_uri=https:%2F%2Fapp.holonym.id/orcid/token/&client_id=${appIDForChain.orcid[newChain]}&scope=openid&nonce=whatever`}}
  />
 }
-/** ORCID **/
-export const ORCILoginButton = (props) => {
 
-  return (
-    <a
-      className="card-link"
-      href={`https://orcid.org/signin?response_type=token&redirect_uri=https:%2F%2Fapp.holonym.id/orcid/token/&client_id=${appIDForChain.orcid[props.desiredchain]}&scope=openid&nonce=whatever"`}
-    >
-      {props.creds ? "Update ORCID" : "Link ORCID"}
-    </a>
-  );
-};
+// export const ORCIDLoginButton = (props) => {
 
-/** Twitter **/
-export const TwitterLoginButton = (props) => {
-  return (
-    <a className="card-link" href={`https://holonym.id/auth/twitter/chain/${props.desiredchain}`}>
-      {props.creds ? "Update Twitter" : "Link Twitter"}
-    </a>
-  );
-};
+//   return (
+//     <a
+//       className="card-link"
+//       href={`https://orcid.org/signin?response_type=token&redirect_uri=https:%2F%2Fapp.holonym.id/orcid/token/&client_id=${appIDForChain.orcid[props.desiredchain]}&scope=openid&nonce=whatever"`}
+//     >
+//       {props.creds ? "Update ORCID" : "Link ORCID"}
+//     </a>
+//   );
+// };
 
-/** GitHub **/
-export const GitHubLoginButton = (props) => {
-  return (
-    <a className="card-link" href={`https://holonym.id/auth/github/chain/${props.desiredchain}`}>
-      {props.creds ? "Update GitHub" : "Link GitHub"}
-    </a>
-  );
-};
+export const TwitterLoginButton = (props) => (
+  <LoginButton 
+    web2service={"Twitter"} 
+    callback={(newChain)=>{window.location.href=`https://holonym.id/auth/twitter/chain/${newChain}`}}
+ />
+)
 
-/** Discord **/
-export const DiscordLoginButton = (props) => {
-  return (
-    <a className="card-link" href={`https://holonym.id/auth/discord/chain/${props.desiredchain}`}>
-      {props.creds ? "Update Discord" : "Link Discord"}
-    </a>
-  );
-};
+export const GitHubLoginButton = (props) => (
+  <LoginButton 
+    web2service={"GitHub"} 
+    callback={(newChain)=>{window.location.href=`https://holonym.id/auth/github/chain/${newChain}`}}
+ />
+)
 
-/** Google **/
-const InnerGoogleLoginButton = (props) => {
-  console.log("propss", props);
-  return (
-    <a className="card-link" onClick={props.onClick} disabled={props.disabled}>
-      {props.creds ? "Update Google" : "Link Google"}
-    </a>
-  );
-};
+export const DiscordLoginButton = (props) => (
+  <LoginButton 
+    web2service={"Discord"} 
+    callback={(newChain)=>{window.location.href=`https://holonym.id/auth/discord/chain/${newChain}`}}
+ />
+)
+// export const TwitterLoginButton = (props) => {
+//   return (
+//     <a className="card-link" href={`https://holonym.id/auth/twitter/chain/${props.desiredchain}`}>
+//       {props.creds ? "Update Twitter" : "Link Twitter"}
+//     </a>
+//   );
+// };
+// export const GitHubLoginButton = (props) => {
+//   return (
+//     <a className="card-link" href={`https://holonym.id/auth/github/chain/${props.desiredchain}`}>
+//       {props.creds ? "Update GitHub" : "Link GitHub"}
+//     </a>
+//   );
+// };
+// export const DiscordLoginButton = (props) => {
+//   return (
+//     <a className="card-link" href={`https://holonym.id/auth/discord/chain/${props.desiredchain}`}>
+//       {props.creds ? "Update Discord" : "Link Discord"}
+//     </a>
+//   );
+// };
 
-// Provides a simple component, GoogleLoginButton, which wraps two more components: the GoogleLogin component from npm package, which renders a custom InnerGoogleLoginButton
-export const GoogleLoginButton = (props) => {
-  const navigate = useNavigate();
-  return (
-    <GoogleLogin
-      clientId="254984500566-3qis54mofeg5edogaujrp8rb7pbp9qtn.apps.googleusercontent.com"
-      render={(renderProps) => <InnerGoogleLoginButton {...renderProps} {...props} />}
-      onSuccess={(r) => navigate(`/google/token/id_token=${r.tokenId}`)}
-    />
-  );
-};
+
+
+
+
+
+
+
+/** Google (unused) **/ 
+// const InnerGoogleLoginButton = (props) => {
+//   console.log("propss", props);
+//   return (
+//     <a className="card-link" onClick={props.onClick} disabled={props.disabled}>
+//       {props.creds ? "Update Google" : "Link Google"}
+//     </a>
+//   );
+// };
+
+// // Provides a simple component, GoogleLoginButton, which wraps two more components: the GoogleLogin component from npm package, which renders a custom InnerGoogleLoginButton
+// export const GoogleLoginButton = (props) => {
+//   const navigate = useNavigate();
+//   return (
+//     <GoogleLogin
+//       clientId="254984500566-3qis54mofeg5edogaujrp8rb7pbp9qtn.apps.googleusercontent.com"
+//       render={(renderProps) => <InnerGoogleLoginButton {...renderProps} {...props} />}
+//       onSuccess={(r) => navigate(`/google/token/id_token=${r.tokenId}`)}
+//     />
+//   );
+// };
